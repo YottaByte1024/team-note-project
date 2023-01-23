7d5d8ac30739d8619e7fc0a70b2543783ebe299a
YottaByte1024 <kirill1999z@hotmail.com>
2023-01-23 22:11:53 +0300

    created project template

---
+++ teamnoteproject/noteapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Note(models.Model):
+    name = models.CharField(max_length=128, default="No name")
+    # path = models.CharField(max_length=256, default="/")
+    text = models.TextField(default="")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of the model.
+        """
+        return reverse('model-detail-view', args=[str(self.id)])
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=256)
+    # head = models.ForeignKey()
+
